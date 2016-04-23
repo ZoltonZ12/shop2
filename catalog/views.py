9c4635bab852d1ccbe34a2b9d339d84e069e5b2d
root <LavrovIV@yandex.ru>
2016-04-23 23:47:19 +0300

    remove local  rep to home dir

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from catalog.models import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.template.loader import render_to_string
+from django.core.context_processors import  csrf
+from django.views.decorators.csrf import csrf_exempt
+from datetime import *
+import random
+import string
 
 # Create your views here.
+def home(request):
+    tovars = []
+    for x in range(0,3):
+        tovars.append(x)
+    context = {
+        'title': 'Helloworld',
+        'tovars': tovars,
+    }
+    return HttpResponse(render_to_string('index.html', context=))
